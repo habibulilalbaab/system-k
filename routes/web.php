9db1839f294ba856ae5bf4898620fb939e9585bd
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2023-08-05 16:18:58 +0700

    dashboard page blank

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/home');
+    return redirect('/dashboard');
 });
+
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
